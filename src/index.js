f505c23fa73b4a3daad1a0a9311ec9ea6c9d2b66
maricegalla <maricegalla@gmail.com>
2022-01-26 16:17:14 -0300

    Created theme and changed favicon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    blue: "#69A3FF",
+    orange: "#FF9632",
+    red:"ED1946",
+    darkGray: "#737C84",
+    mediumGray: "#B4B4B4",
+    lightGray: {
+      1:"rgba(241, 241, 241, 0.5)",
+      2:"#F1F1F1",
+      3:"EBEBEB",
+      4:"rgba(51, 51, 51, 0.9)"
+    }
+  },
+  fonts: {
+    body: "Arial, Helvetica, sans-serif",
+  },
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
